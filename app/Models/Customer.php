8b33a34952d47536a3049c467d6f9e076b85f43a
Rody Huancas <rodyhuancas.04@gmail.com>
2025-07-23 18:43:53 -0500

    Feat: Add Invoice model and Customer relationship

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,4 +9,15 @@ class Customer extends Model
 {
     /** @use HasFactory<\Database\Factories\CustomerFactory> */
     use HasFactory;
+    protected $fillable = [];
+
+    /**
+     * Get all of the invoices for the Customer
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
---
+++ app/Models/Invoice.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Invoice extends Model
+{
+    /** @use HasFactory<\Database\Factories\InvoiceFactory> */
+    use HasFactory;
+    protected $fillable = [];
+
+    /**
+     * Get the customer that owns the Invoice
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
